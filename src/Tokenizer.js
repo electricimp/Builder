9568443ca2c6ec8f1a0e197b32bf67527bec626b
Mikhail Yurasov <me@yurasov.me>
2016-03-26 21:45:02 -0700

    Update tokenizer

--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -5,49 +5,75 @@
 
 'use strict';
 
-const TOKENS = {
-  T_INCLUDE: 'include',
-  T_DEFINE: 'define',
-  T_UNDEFINE: 'undefine',
-  T_IF: 'if',
-  T_ELSE: 'else',
-  T_ELSEIF: 'elseif',
-  T_ENDIF: 'endif',
-  T_SOURCE_LINE: 'source_line'
-};
-
-const REGEXPS = {
-  [TOKENS.T_INCLUDE]: /^@include +(.*)$/,
-  [TOKENS.T_DEFINE]: /^@define +(.*)$/,
-  [TOKENS.T_UNDEFINE]: /^@undefine +(.*)$/,
-  [TOKENS.T_IF]: /^@if +(.*)$/,
-  [TOKENS.ELSE]: /^@else *$/,
-  [TOKENS.ELSEIF]: /^@elseif +(.*)$/,
-  [TOKENS.T_ENDIF]: /^@endif *$/
+// token types
+const tokens = {
+  INCLUDE: 'include',
+  DEFINE: 'define',
+  UNDEFINE: 'undefine',
+  IF: 'if',
+  ELSE: 'else',
+  ELSEIF: 'elseif',
+  ENDIF: 'endif',
+  SOURCE_LINE: 'source_line'
 };
 
 class Tokenizer {
-  static tokenizeLine(line) {
-    const trimmedLine = line.trim();
 
-    // iterate through regeps
-    for (const token in REGEXPS) {
-      let m;
-      const r = REGEXPS[token];
-      if (m = trimmedLine.match(r)) {
-        return {
-          token: token,
-          value: m[1]
-        };
-      }
+  /**
+   * Tokenize a string
+   * @param {string} content
+   * @return {Array}
+   */
+  static tokenize(content) {
+    const res = [];
+
+    for (const line of content.toString().split(/\n|\r\n/)) {
+      res.push(this.tokenizeLine(line));
     }
 
-    return {
-      token: TOKENS.T_SOURCE_LINE,
-      value: line
-    };
+    return res;
+  }
+
+  /**
+   * Tokenize a single line
+   * @param {string} line
+   * @return {{value, token}}
+   */
+  static tokenizeLine(line) {
+    let m;
+
+    if (m = line.trim().match(/^@(include|define|undefine|if|else|elseif|endif)\b(.*)$/)) {
+
+      const keyword = m[1];
+      const value = m[2].trim();
+
+      switch (keyword) {
+
+        // @include <source:expression>
+        case 'include':
+          return {token: tokens.INCLUDE, source: value};
+
+        case 'define':
+
+          // @define <variable:varname> <value:expression>
+          if (m = value.match(/^([_A-Za-z][_A-Za-z0-9]*)\s+(.*)$/)) {
+            return {token: tokens.DEFINE, varname: m[1], value: m[2]};
+          } else {
+            throw new Error('Syntax error in @define');
+          }
+
+          break;
+
+      }
+
+    } else {
+      return {
+        token: tokens.SOURCE_LINE,
+        line
+      };
+    }
   }
 }
 
 module.exports = Tokenizer;
-module.exports.TOKENS = TOKENS;
+module.exports.tokens = tokens;
