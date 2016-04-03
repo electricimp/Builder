3cfabb5ecb9d9e1bf2dfc2cf228b5e74a5225ea8
Mikhail Yurasov <me@yurasov.me>
2016-04-03 01:47:42 -0700

    Add spec on sample #1

--- spec/AstParser/basic.spec.js
+++ spec/AstParser/basic.spec.js
@@ -7,7 +7,7 @@
 
 require('jasmine-expect');
 
-const AstParser = require('../src/AstParser');
+const AstParser = require('../../src/AstParser');
 
 describe('AstParser', () => {
   let p;
@@ -31,8 +31,8 @@ describe('AstParser', () => {
   //noinspection Eslint
     expect(r).toEqual([{'line':1,'file':'main','type':'source_line','value':'// main 1'},{'line':2,'file':'main','type':'if','test':'1','consequent':[{'line':3,'file':'main','type':'source_line','value':'// then:1'}],'alternate':[{'line':5,'file':'main','type':'source_line','value':'// else:1'}]},{'line':7,'file':'main','type':'source_line','value':'// main (last)'}]);
 
-    console.error(JSON.stringify(r).replace(/\"/g, '\''));
-    console.error(JSON.stringify(r, null, '  '));
+    // console.error(JSON.stringify(r).replace(/\"/g, '\''));
+    // console.error(JSON.stringify(r, null, '  '));
   });
 
   //noinspection Eslint
@@ -54,8 +54,8 @@ describe('AstParser', () => {
 // main (last)`
     );
 
-    console.error(JSON.stringify(r).replace(/\"/g, '\''));
-    console.error(JSON.stringify(r, null, '  '));
+    // console.error(JSON.stringify(r).replace(/\"/g, '\''));
+    // console.error(JSON.stringify(r, null, '  '));
 
     //noinspection Eslint
     expect(r).toEqual([{'line':1,'file':'main','type':'source_line','value':'// main 1'},{'line':2,'file':'main','type':'if','test':'1','consequent':[{'line':3,'file':'main','type':'source_line','value':'  // then:1'}],'elseifs':[{'line':4,'file':'main','type':'if','test':'1','consequent':[{'line':5,'file':'main','type':'source_line','value':'  // elseif 1:1'},{'line':6,'file':'main','type':'source_line','value':'  // elseif 1:2'},{'line':7,'file':'main','type':'source_line','value':'  // elseif 1:3'}]},{'line':8,'file':'main','type':'if','test':'2','consequent':[{'line':9,'file':'main','type':'source_line','value':'  // elseif 2:1'},{'line':10,'file':'main','type':'source_line','value':'  // elseif 2:2'}]}],'alternate':[{'line':12,'file':'main','type':'source_line','value':'  // else:1'}]},{'line':14,'file':'main','type':'source_line','value':'// main (last)'}]);
--- spec/fixtures/sample-1/input.nut
+++ spec/fixtures/sample-1/input.nut
@@ -5,9 +5,16 @@
 
 @if SOMEVAR1 == 123
   // should be included
+
+  @if level2
+    //
+    @set abc def
+  @endif
+
 @elseif SOMEVAR2 == 0
   // should not be included
 @elseif SOMEVAR2 == 256
+  // should be included
   // should be included
 @endif
 
