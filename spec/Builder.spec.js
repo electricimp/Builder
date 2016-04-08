c5d8fc58e1124233329613426e71379d8d74525b
Mikhail Yurasov <me@yurasov.me>
2016-04-08 00:43:14 -0700

    + Builder spec

---
+++ spec/Builder.spec.js
@@ -0,0 +1,26 @@
+/**
+ * Expression spec
+ * @author Mikhail Yurasov <mikhail@electricimp.com>
+ */
+
+'use strict';
+
+const Builder = require('../src');
+const Machine = require('../src/Machine');
+
+describe('Expression', () => {
+
+  let builder;
+
+  beforeEach(() => {
+    builder = new Builder();
+    builder.machine.generateLineControlStatements = true;
+  });
+
+  it('should build something', () => {
+    expect(builder.machine instanceof Machine).toBeTruthy();
+    expect(builder.machine.execute('@{__FILE__}:@{__LINE__}'))
+      .toBe('#line 1 "main"\nmain:1');
+  });
+
+});
