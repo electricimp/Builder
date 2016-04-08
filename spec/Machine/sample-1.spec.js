70a66e82539c944766f331e5398a14cf4e001197
Mikhail Yurasov <me@yurasov.me>
2016-04-08 00:15:20 -0700

    Add spec on line control

--- spec/Machine/sample-1.spec.js
+++ spec/Machine/sample-1.spec.js
@@ -11,14 +11,17 @@ const FILE = __dirname + '/../fixtures/sample-1/input.nut';
 const init = require('./init')(FILE);
 
 describe('Machine', () => {
-  let machine, result;
+  let machine, result, resultWithLC;
 
   beforeEach(() => {
     machine = init.createMachine();
     result = init.getResult();
+    resultWithLC = init.getResultWithLineControl();
   });
 
   it('should do something alright #1', () => {
     expect(machine.execute('@include "input.nut"')).toBe(result);
+    machine.generateLineControlStatements = true;
+    expect(machine.execute('@include "input.nut"')).toBe(resultWithLC);
   });
 });
