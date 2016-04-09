64b0302e987e44112b55ba18c016b2db078ec687
Mikhail Yurasov <me@yurasov.me>
2016-04-08 18:21:59 -0700

    Update macro expressions spec

--- spec/MacroExpression.spec.js
+++ spec/MacroExpression.spec.js
@@ -15,9 +15,36 @@ describe('MacroExpression', () => {
     e = new MacroExpression();
   });
 
-  it('should parse macro definitions', () => {
-    e.parseDefinition('macro1(arg1, arg2)');
-    console.log(e);
+  it('should parse macro declarations #1', () => {
+    e.parseDeclaration('macro1(arg1, arg2)');
+    expect(e.macroName).toBe('macro1');
+    expect(e.args).toEqual(['arg1', 'arg2']);
+  });
+
+  it('should parse macro declarations #2', () => {
+    e.parseDeclaration('macro1()');
+    expect(e.macroName).toBe('macro1');
+    expect(e.args).toEqual([]);
+  });
+
+  it('should fail on syntax error #1', () => {
+    try {
+      e.parseDeclaration('macro1');
+      fail();
+    } catch  (e) {
+      expect(e instanceof MacroExpression.Errors.SyntaxError).toBeTruthy();
+      expect(e.message).toBe('Syntax error in macro declaration');
+    }
+  });
+
+  it('should fail on syntax error #2', () => {
+    try {
+      e.parseDeclaration('macro1(1)');
+      fail();
+    } catch  (e) {
+      expect(e instanceof MacroExpression.Errors.SyntaxError).toBeTruthy();
+      expect(e.message).toBe('Syntax error in macro declaration');
+    }
   });
 
 });
