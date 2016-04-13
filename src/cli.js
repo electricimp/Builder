2736c11a9ae054943367582a6d316d866d434870
Mikhail Yurasov <me@yurasov.me>
2016-04-13 03:16:19 -0700

    Initial CLI

---
+++ src/cli.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+/**
+ * Basic CLI for testing
+ *
+ * Usage:
+ *  <this> input_file [-l]
+ *
+ *  input_file  - File to process
+ *  -l          - Generate line control statements
+ *
+ * @author Mikhail Yurasov <me@yurasov.me>
+ */
+
+'use strict';
