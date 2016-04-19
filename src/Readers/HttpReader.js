df247b6679a11a4d63d33b2db7ef67379788701e
Mikhail Yurasov <me@yurasov.me>
2016-04-19 16:18:39 -0700

    Spawning reader

--- src/Readers/HttpReader.js
+++ src/Readers/HttpReader.js
@@ -6,7 +6,19 @@
 'use strict';
 
 const request = require('request');
+const childProcess = require('child_process');
 const AbstractReader = require('./AbstractReader');
+
+// child process timeout
+const TIMEOUT = 30000;
+
+// return codes
+const STATUS_FETCH_FAILED = 2;
+const STATUS_HTTP_ERROR = 3;
+
+// marker presense on the command line
+// tells that we're in the woker thread
+const WORKER_MARKER = '__http_reader_worker___';
 
 class HttpReader extends AbstractReader {
 
@@ -21,24 +33,72 @@ class HttpReader extends AbstractReader {
    */
   read(url) {
 
-    request.get(url, (error, response, body) => {
+    // spawn child process
+    const child = childProcess.spawnSync(
+      /* node */ process.argv[0],
+      /* self */ [__filename, WORKER_MARKER, url],
+      {timeout: TIMEOUT}
+    );
 
-      if (error) {
-        throw new AbstractReader.Errors.SourceReadingError(
-          `Failed to fetch url "${url}": ${error}`
-        );
-      } else if (response.statusCode >= 400) {
-        throw new AbstractReader.Errors.SourceReadingError(
-          `Failed to fetch url "${url}": ${response.statusCode}`
-        );
+    if (STATUS_FETCH_FAILED === child.status || STATUS_HTTP_ERROR === child.status) {
+
+      // predefined exit code errors
+      throw new AbstractReader.Errors.SourceReadingError(child.stderr.toString());
+
+    } else if (0 !== child.status) {
+
+      // misc exit code errors
+      throw new AbstractReader.Errors.SourceReadingError(`Unknown error: ${child.stderr.stoString()} (exit code ${child.status})`);
+
+    } else {
+
+      // errors that do not set erroneous exit code
+      if (child.error) {
+
+        if (child.error.errno === 'ETIMEDOUT') {
+
+          // timeout
+          throw new AbstractReader.Errors.SourceReadingError(`Failed to fetch url "${url}": timed out after ${TIMEOUT / 1000}s`);
+
+        } else {
+
+          // others
+          throw new AbstractReader.Errors.SourceReadingError(`Failed to fetch url "${url}": ${child.error.errno}`);
+
+        }
+
       } else {
-
-
-
+        // s'all good
+        return child.output[1].toString();
       }
 
+    }
+  }
+
+  /**
+   * Fethces the url and outputs it to STDOUT
+   * @param {string} url
+   */
+  static fetchUrl(url) {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        process.stderr.write(`Failed to fetch url "${url}": ${error}`);
+        process.exit(STATUS_FETCH_FAILED);
+      } else if (response.statusCode >= 400) {
+        process.stderr.write(`Failed to fetch url "${url}": HTTP/${response.statusCode}`);
+        process.exit(STATUS_HTTP_ERROR);
+      } else {
+        process.stdout.write(body);
+      }
     });
   }
 }
 
-module.exports = HttpReader;
+if (process.argv.indexOf(WORKER_MARKER) !== -1) {
+  // launch worker
+  HttpReader.fetchUrl(process.argv[3]);
+} else {
+  // acto as module
+  module.exports = HttpReader;
+}
+
