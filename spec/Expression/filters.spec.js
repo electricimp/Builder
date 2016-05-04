1a60d25b9b6b261ca5df7323eb52c52845e5ccec
Mikhail Yurasov <me@yurasov.me>
2016-05-03 22:43:53 -0700

    Spec on filters in expressions

--- spec/Expression/filters.spec.js
+++ spec/Expression/filters.spec.js
@@ -8,6 +8,7 @@
 require('jasmine-expect');
 
 const Expression = require('../../src/Expression');
+const EscapeFilter = require('../../src/Filters/EscapeFilter');
 
 describe('Expression', () => {
 
@@ -15,11 +16,12 @@ describe('Expression', () => {
 
   beforeEach(() => {
     expression = new Expression();
+    expression.filters['escape'] = new EscapeFilter();
   });
 
-  // it('should fail to parse non-macro call expression', () => {
-  //   const res = expression.parseFiltersCall('expression|filters', {}, {'macro1': {}});
-  //   expect(res).toBe(null);
-  // });
+  it('apply filter #1', () => {
+    const res = expression.evaluate('"something"|escape');
+    expect(res).toBe(null);
+  });
 
 });
