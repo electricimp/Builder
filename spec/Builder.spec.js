3ad596a1808039fedfb703d9774a27389f6006d0
Mikhail Yurasov <me@yurasov.me>
2016-05-08 21:18:26 -0700

    Spec on "escape" filter

--- spec/Builder.spec.js
+++ spec/Builder.spec.js
@@ -10,15 +10,25 @@ const Machine = require('../src/Machine');
 
 describe('Builder', () => {
 
-  const builder = new Builder();
-  builder.machine.generateLineControlStatements = true;
-  builder.machine.readers.github.username = process.env.SPEC_GITHUB_USERNAME;
-  builder.machine.readers.github.password = process.env.SPEC_GITHUB_PASSWORD || process.env.SPEC_GITHUB_TOKEN;
+  let builder;
+
+  beforeEach(() => {
+    builder = new Builder();
+    builder.machine.readers.github.username = process.env.SPEC_GITHUB_USERNAME;
+    builder.machine.readers.github.password = process.env.SPEC_GITHUB_PASSWORD || process.env.SPEC_GITHUB_TOKEN;
+  });
 
   it('should build something', () => {
     expect(builder.machine instanceof Machine).toBeTruthy();
+    builder.machine.generateLineControlStatements = true;
     expect(builder.machine.execute('@{__FILE__}:@{__LINE__}'))
       .toBe('#line 1 "main"\nmain:1');
+  });
+
+  it('should execute "escape" filter #1', () => {
+    expect(builder.machine instanceof Machine).toBeTruthy();
+    const res = builder.machine.execute(`"@{'"'|escape}"`);
+    expect(res).toBe(`"\\""`);
   });
 
 });
