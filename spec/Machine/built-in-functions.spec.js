1fb3ef6c848d2746792bbe842541f3c0e83ff80e
Mikhail Yurasov <me@yurasov.me>
2016-05-10 22:13:15 -0700

    Update spec on built-in functions

--- spec/Machine/built-in-functions.spec.js
+++ spec/Machine/built-in-functions.spec.js
@@ -7,6 +7,7 @@
 
 require('jasmine-expect');
 const init = require('./init')('main');
+const Machine = require('../../src/Machine');
 
 describe('Machine', () => {
   let machine;
@@ -15,12 +16,20 @@ describe('Machine', () => {
     machine = init.createMachine();
   });
 
-  it('should handle built-in functions #1', () => {
+  it('should handle built-in function include()', () => {
     const res = machine.execute(
-`@{include('abc')}`
+`@{include('${__dirname + '/../fixtures/lib/d.builder'}')|escape}`
 );
+    expect(res).toEqual(`d.builder\\nd.builder:2`);
+  });
 
-    console.log(res);
-    // expect(res).toEqual(`a.builder\nb.builder\nc.builder\n`);
+  it('should handle errors in include() calls', () => {
+    try {
+      machine.execute(`\n@{include()}`);
+      fail();
+    } catch (e) {
+      expect(e instanceof Machine.Errors.ExpressionEvaluationError).toBe(true);
+      expect(e.message).toBe('Wrong number of arguments for include() (main:2)');
+    }
   });
 });
