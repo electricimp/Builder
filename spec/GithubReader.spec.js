cd163b7a68ee253206a4159f2e2aa8e6d226d886
Pavel Petroshenko <pavel@electricimp.com>
2017-03-02 11:12:27 -0800

    Electric Imp copyrights.

--- spec/GithubReader.spec.js
+++ spec/GithubReader.spec.js
@@ -1,7 +1,6 @@
-/**
- * GithubReader spec
- * @author Mikhail Yurasov <me@yurasov.me>
- */
+// Copyright (c) 2016-2017 Electric Imp
+// This file is licensed under the MIT License
+// http://opensource.org/licenses/MIT
 
 'use strict';
 
