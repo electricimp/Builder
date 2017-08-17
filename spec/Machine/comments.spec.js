b146f88fe338d88448c9315cc1ecc0d1be64e0ce
Pavel Petroshenko <pavel@electricimp.com>
2017-08-17 10:45:05 -0700

    Updated headers.

---
+++ spec/Machine/comments.spec.js
@@ -0,0 +1,70 @@
+// MIT License
+//
+// Copyright 2017 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+require('jasmine-expect');
+const init = require('./init')('main');
+const jasmineDiffMatchers = require('jasmine-diff-matchers');
+
+describe('Machine', () => {
+  let machine;
+
+  beforeEach(() => {
+    machine = init.createMachine();
+    // show string diffs
+    jasmine.addMatchers(jasmineDiffMatchers.diffChars);
+  });
+
+  it('should handle comments in include corectly', () => {
+    const ans = 'a.builder\n';
+    expect(machine.execute(`@include "${__dirname}/../fixtures/" + "lib/a.builder" // comment`)).toEqual(ans);
+    expect(machine.execute(`@include "${__dirname}/../fixtures//" + '//lib/a.builder' // comment`)).toEqual(ans);
+    expect(machine.execute(`@include "${__dirname}/../fixtures/lib/a.builder"//comment+/comment+"some more comment"`)).toEqual(ans);
+    expect(machine.execute(`@include "${__dirname}/../fixtures/" + 'lib/a.builder' // comment with //`)).toEqual(ans);
+    expect(machine.execute(`@include "${__dirname}/../fixtures/lib/a.builder" // comment with some expr (1 | 0)`)).toEqual(ans);
+    expect(machine.execute(`@include "${__dirname}/../fixtures/" + 'lib/a.builder' // comment with " ' ( ] {`)).toEqual(ans);
+  });
+
+    it('should handle @while corectly #1', () => {
+        const res = machine.execute(
+            `
+@if METER_PROTOCOL_REV == 2.02  // comment 1
+  @set PERIODIC_DATA_PERIOD = 200 // comment 2
+@else // comment 3
+  @set METER_PROTOCOL_REV 1.07 // comment 4
+  @set PERIODIC_DATA_PERIOD = 1
+@end
+@{PERIODIC_DATA_PERIOD} @{METER_PROTOCOL_REV}
+`
+        );
+
+        expect(res).diffChars(
+            `
+1 1.07
+`
+        );
+    });
+
+});
--- src/AstParser.js
+++ src/AstParser.js
@@ -1,6 +1,26 @@
-// Copyright (c) 2016-2017 Electric Imp
-// This file is licensed under the MIT License
-// http://opensource.org/licenses/MIT
+// MIT License
+//
+// Copyright 2016-2017 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 'use strict';
 
@@ -111,11 +131,12 @@ class AstParser {
         arg = matches[2].trim();
         token.args = [];
 
+        // remove single line comments from arg
+        arg = decomment.text(arg);
+
         switch (type) {
 
           case 'include':
-            // remove single line comments from arg
-            arg = decomment.text(arg);
             // detect "once" flag
             if (/^once\b/.test(arg)) {
               token.args.push('once');
