3bc0bef7187f08ee4d5a7a4aa2c437a04a7b78aa
Jaye Heffernan <jpeheffernan@gmail.com>
2017-10-26 23:10:48 +1100

    Add test based on Pull Request feedback

---
+++ spec/fixtures/sample-14/lib.js
@@ -0,0 +1,6 @@
+module.exports = {
+  upper: (s) => s.toUpperCase(),
+  repeat: (s, count) => s.repeat(count),
+  split: (s, seperator, index) => s.split(seperator)[index],
+  length: (s) => s.length,
+};
