3b120fc60e25c14c1bbd8d6b8ea9a121b4842fb7
Ruslan Ragimov <ruslan.ragimov@nobitlost.com>
2020-05-21 21:43:01 +0300

    A bit of refactoring and bug fixes

--- src/Readers/AzureReposReader.js
+++ src/Readers/AzureReposReader.js
@@ -30,6 +30,8 @@ const upath = require('upath');
 const childProcess = require('child_process');
 const AbstractReader = require('./AbstractReader');
 
+// Azure DevOps services API url
+const AZURE_API_URL = "https://dev.azure.com/";
 // Child process timeout
 const TIMEOUT = 30000;
 // Child process error return code
@@ -81,8 +83,7 @@ class AzureReposReader extends AbstractReader {
         source,
         this.username,
         this.token,
-        commitID,
-        needCommitID
+        commitID
       ],
       { timeout: this.timeout }
     );
@@ -150,7 +151,7 @@ class AzureReposReader extends AbstractReader {
    * @param {string} password - password or token (if required)
    * @return {{data}}
    */
-  static fetch(source, username, password, commitID, needCommitID) {
+  static fetch(source, username, password, commitID) {
     var auth = null;
 
     if (username !== '' && password !== '') {
@@ -162,16 +163,15 @@ class AzureReposReader extends AbstractReader {
     }
 
     const sourceParsed = this.parseUrl(source);
-    const promise = AzureReposReader.downloadFile(auth, sourceParsed, commitID, needCommitID);
 
-    promise.then(function(result) {
-      const data = JSON.parse(result);
-      const ret = {
-        data: data.content,
-        commitID: data.commitId
-      };
-      process.stdout.write(JSON.stringify(ret));
-    });
+    AzureReposReader.downloadFile(auth, sourceParsed, commitID)
+      .then(function(result) {
+        const ret = {
+          data: result.content,
+          commitID: result.commitId
+        };
+        process.stdout.write(JSON.stringify(ret));
+      });
   }
 
   /**
@@ -179,65 +179,91 @@ class AzureReposReader extends AbstractReader {
    * @param {object} sourceParsed - parsed source URI
    * @return {Promise}
    */
-  static downloadFile(auth, sourceParsed, commitID, needCommitID) {
-    var url = null;
+  static downloadFile(auth, sourceParsed, commitID) {
+    sourceParsed.path = upath.normalize(sourceParsed.path);
+
+    var baseUrl = AZURE_API_URL + `${sourceParsed.org}/${sourceParsed.project}` +
+                  `/_apis/git/repositories/${sourceParsed.repo}/items?path=${sourceParsed.path}` +
+                  `&api-version=5.1&$format=json&includeContent=true`;
 
     var params = {
-      url: url,
-      auth: auth
+      url: baseUrl,
+      auth: auth,
+      json: true
     };
 
-    url = "https://dev.azure.com/" + sourceParsed.org + "/" + sourceParsed.project
-        + "/_apis/git/repositories/" + sourceParsed.repo + "/items?path=" + sourceParsed.path
-        + "&api-version=5.1&$format=json&includeContent=true";
-
-    if (sourceParsed.ref && commitID === 'null') {
-      url += "&versionDescriptor.version=" + sourceParsed.ref + "&versionDescriptor.versionType=branch";
-    // use-dependencies option
-    } else if (commitID !== 'null') {
-      url += "&versionDescriptor.version=" + commitID + "&versionDescriptor.versionType=commit";
+    // If we have a specific commit id from the dependencies file use-dependencies option is on and there is an appropriate record in the dependencies
+    if (commitID !== 'null') {
+      params.url += `&versionDescriptor.version=${commitID}&versionDescriptor.versionType=commit`;
+      return AzureReposReader.httpGet(params);
     }
-    params.url = url;
 
+    if (sourceParsed.ref) {
+      baseUrl += `&versionDescriptor.version=${sourceParsed.ref}`;
+      // Try the ref as a branch name
+      params.url = baseUrl + "&versionDescriptor.versionType=branch";
+      // Will be rejected if not found
+      return AzureReposReader.httpGet(params, true)
+        .catch(() => {
+          // Try the ref as a tag
+          params.url = baseUrl + "&versionDescriptor.versionType=tag";
+          // Will also be rejected if not found
+          return AzureReposReader.httpGet(params, true);
+        })
+        .catch(() => {
+          // Try the ref as a commit id
+          params.url = baseUrl + "&versionDescriptor.versionType=commit";
+          // Will terminate the process if not found or any other error occurred
+          return AzureReposReader.httpGet(params, false);
+        });
+    }
+
+    return AzureReposReader.httpGet(params);
+  }
+
+  /**
+   * Makes an HTTP GET request. In case of an error terminates the process
+   * (except the 404 Not found error when allowNotFound option used - in this case the returned promise is rejected)
+   * @param {object} params - request's parameters (url, auth, ...)
+   * @param {boolean} [allowNotFound=false] - if true, just reject the returned promise in case of 404 Not found error
+   * @return {Promise}
+   */
+  static httpGet(params, allowNotFound = false) {
     return new Promise(function(resolve, reject) {
-      request.get(params, (error, resp, body) => {
-        if (sourceParsed.ref && commitID === 'null' && resp.statusCode == 404) {
-          // If branch is not found, trying ref as tag
-          url = url.replace("&versionDescriptor.versionType=branch", "&versionDescriptor.versionType=tag");
-          params.url = url;
-          request.get(params, (error, resp, body) => {
-            if (resp.statusCode == 404) {
-              // If tag is not found, trying ref as commit
-              url = url.replace("&versionDescriptor.versionType=tag", "&versionDescriptor.versionType=commit");
-              params.url = url;
-              request.get(params, (error, resp, body) => {
-                AzureReposReader.checkResponse(url, error, resp);
-                resolve(body);
-              });
-            } else {
-              AzureReposReader.checkResponse(url, error, resp);
-              resolve(body);
-            }
-          });
+      const getCb = (error, resp, body) => {
+        // Handle the response. This will:
+        // - Return the response's body in case of success
+        // - Return null in case of 404 Not found error if the allowNotFound option used
+        // - Terminate the process otherwise
+        const result = AzureReposReader.handleResponse(params.url, error, resp, allowNotFound);
+
+        if (result) {
+          resolve(result);
         } else {
-          AzureReposReader.checkResponse(url, error, resp);
-          resolve(body);
+          reject();
         }
-      });
+      };
+
+      request.get(params, getCb);
     });
   }
 
   /**
-   * Checks the response of an HTTP request. Terminates the process in case of an error
+   * Checks the response of an HTTP request and returns the response's body in case of success. Terminates the process
+   * in case of an error (except the 404 Not found error when allowNotFound option used - in this case returns null)
    * @param {string} url - request URL
    * @param {string} error - error message
    * @param {string} resp - response of the request
+   * @param {boolean} [allowNotFound=false] - if true, just return null in case of 404 Not found error
+   * @return {object|null}
    */
-  static checkResponse(url, error, resp) {
+  static handleResponse(url, error, resp, allowNotFound = false) {
     try {
       if (error) {
         process.stderr.write(`Failed to fetch url "${url}": ${error}\n`);
         process.exit(STATUS_FETCH_FAILED);
+      } else if (resp.statusCode == 404 && allowNotFound) {
+        return null;
       } else if (resp.statusCode < 200 || resp.statusCode >= 300) {
         process.stderr.write(`Failed to fetch url "${url}": HTTP/${resp.statusCode}\n`);
 
@@ -250,6 +276,8 @@ class AzureReposReader extends AbstractReader {
       process.stderr.write(`Failed to fetch url "${url}": ${err}\n`);
       process.exit(STATUS_FETCH_FAILED);
     }
+
+    return resp.body;
   }
 
   /**
@@ -284,7 +312,7 @@ class AzureReposReader extends AbstractReader {
 
 if (process.argv.indexOf(WORKER_MARKER) !== -1) {
   // Launch worker
-  AzureReposReader.fetch(process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7]);
+  AzureReposReader.fetch(process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
 } else {
   module.exports = AzureReposReader;
 }
